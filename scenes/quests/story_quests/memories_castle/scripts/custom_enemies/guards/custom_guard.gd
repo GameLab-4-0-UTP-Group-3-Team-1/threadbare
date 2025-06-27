22152d457705e4c2935d56d2e7ca4a20eb68d84c
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2025-06-27 13:47:59 -0500

    Arreglando problemas

--- scenes/quests/story_quests/memories_castle/scripts/custom_enemies/guards/custom_guard.gd
+++ scenes/quests/story_quests/memories_castle/scripts/custom_enemies/guards/custom_guard.gd
@@ -3,11 +3,16 @@ class_name CustomGuard extends Guard
 @export var max_health: float = 100
 var health: float = max_health
 
+@export var duration_animation_defeated: float = 0.5
+
 func recive_damage(lost_live = 0) -> void:
 	health -= lost_live
 	if health <= 0:
 		self.remove()
 
 func remove()  -> void:
+	sprite.play(&"defeated")
+	await get_tree().create_timer(duration_animation_defeated).timeout
+	
 	set_physics_process(false)
 	self.queue_free()
